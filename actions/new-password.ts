060d8ae5bee384dbf8aba09a5df0226ff6bcb46e
Satyam Raj <satyamrajgaya434@gmail.com>
2025-03-15 01:32:21 +0530

    .

---
+++ actions/new-password.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { getPasswordResetTokenByToken } from "@/data/password-reset-token";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/lib/db";
+import { NewPasswordSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
+import * as z from "zod";
+
+export const newPassword = async (
+  values: z.infer<typeof NewPasswordSchema>,
+  token?: string | null,
+) => {
+  const validatedFields = NewPasswordSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { password } = validatedFields.data;
+
+  if (!token) {
+    return { error: "Missing Token!" };
+  }
+
+  const exisitingToken = await getPasswordResetTokenByToken(token);
+
+  if (!exisitingToken) {
+    return { error: "Invalid token!" };
+  }
+
+  const hasExpired = new Date(exisitingToken.expires) < new Date();
+
+  if (hasExpired) {
+    return { error: "Token has expired!" };
+  }
+
+  const existingUser = await getUserByEmail(exisitingToken.email);
+
+  if (!existingUser) {
+    return { error: "Email does not exist!" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.update({
+    where: { id: existingUser.id },
+    data: {
+      password: hashedPassword,
+    },
+  });
+
+  await db.passwordResetToken.delete({
+    where: { id: exisitingToken.id },
+  });
+
+  return { success: "Password Updated!" };
+};
---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { auth } from "@/auth";
+import { SessionProvider } from "next-auth/react";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Get started with Create Next App",
+};
+
+export { metadata };
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session = await auth();
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <SessionProvider session={session}>
+          <Toaster />
+          {children}
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
